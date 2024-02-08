41e6efe9071c1de0b891ec428e39524cc73a1a8e
Siouyu Hsu <siouyu@SiouyudeMacBook-Air.local>
2024-02-08 12:31:58 +0800

    Finish Day39 on Feb 8,2024

---
+++ Day39_PasswordStrength/script.js
@@ -0,0 +1,11 @@
+const password = document.getElementById("password");
+const background = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+	const input = e.target.value;
+	// console.log(input);
+	const length = input.length;
+	// console.log(length);
+	const blurValue = 20 - length * 2;
+	background.style.filter = `blur(${blurValue}px)`;
+});
